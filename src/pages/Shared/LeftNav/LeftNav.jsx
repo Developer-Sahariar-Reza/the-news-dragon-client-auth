757884f55c954d70bd7c4cd068d36a5ee8d8fcc0
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-09-06 21:03:41 +0600

    node express added and left nav categories added

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LeftNav.css";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.error(error));
+  }, []);
   return (
-    <div>
-      <h1>Left Nav</h1>
+    <div className="left-nav">
+      <div className="category-section">
+        <h1 className="category-title">All Categories</h1>
+        <ul>
+          {categories.map((category) => (
+            <li key={category.id}>
+              <Link>{category.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
