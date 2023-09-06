af14858c5043b5bbd39770a5ba1909fe8779d3e5
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-09-06 17:02:01 +0600

    React fast marquee implement and navbar added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Header.css";
 import moment from "moment";
+import Marquee from "react-fast-marquee";
 import logo from "../../../assets/images/logo.png";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -17,6 +19,56 @@ const Header = () => {
       <div className="header-time">
         <p>{moment().format("dddd, MMMM Do YYYY")}</p>
       </div>
+
+      <div className="header-marquee">
+        <button>Latest</button>
+        <Marquee
+          className="header-marquee-text"
+          speed={150}
+          pauseOnHover={true}
+        >
+          Match Highlights: Germany vs Spain — as it happened ! Match
+          Highlights: Germany vs Spain as...
+        </Marquee>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "not-active")}
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "active" : "not-active")}
+            >
+              About
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/career"
+              className={({ isActive }) => (isActive ? "active" : "not-active")}
+            >
+              Career
+            </NavLink>
+          </li>
+        </ul>
+
+        <div className="nav-info">
+          <p>you@email.com</p>
+          <Link>
+            <button className="common-button">Login</button>
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 };
